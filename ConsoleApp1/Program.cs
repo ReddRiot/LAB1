a4b4d14228ebf84ea88d1c60326c649a86b182cf
Kapusenko Taisiia-Valeria <taya.kapusenko2001@gmail.com>
2020-09-23 17:48:19 +0300

    fibonacci

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num = Input("How many Fibonacci numbers do you wish to print?");
+            Console.WriteLine("====================================");
+            int c = 0;
+            int a = 0;
+            int b = 1;
+            int counter = 0;
+            while (counter != num)
+            {
+                Console.WriteLine(b);
+                c = b;
+                b = a + b;
+                a = c;
+                counter += 1;
+            }
+            Console.ReadKey();
+        }
+
+        static int Input(string mes)
+        {
+            Console.WriteLine(mes);
+            return Int32.Parse(Console.ReadLine());
+        }
+    }
+   
+    
+}
